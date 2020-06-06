946bec11a8c7f8e28808dfb7ec4086450638cfcc
XPS-9560\Dorian <popa.dorian98@gmail.com>
2020-06-06 17:22:10 +0300

    Added filed validation for the AuthenticationModel

--- Ardent-API/Models/AuthenticationModel.cs
+++ Ardent-API/Models/AuthenticationModel.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Ardent_API.Models
 {
     public class AuthenticationModel
     {
+        [Required]
+        [StringLength(10, ErrorMessage = "Username length can't be more than 10.")]
         public string Username { get; set; }
+
+        [Required]
         public string PasswordPlain { get; set; }
     }
 }
